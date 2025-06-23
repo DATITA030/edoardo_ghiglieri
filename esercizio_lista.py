51638778082ef29ec47eab37b6530a6b10c03b64
Edoardo Ghiglieri <edoghiro2@gmail.com>
2025-06-23 14:15:14 +0200

    secondo esercizio esercizio_lista finito

--- esercizio_lista.py
+++ esercizio_lista.py
@@ -34,3 +34,18 @@ index_max = numeri.index (max(numeri))
 index_min = numeri.index (min(numeri))
 print  (index_max)
 print  (index_min)
+
+#esercizio due:
+voti=[]
+flag = True
+while flag==True:
+    voti.append (int(input('inserisci voto:')))
+    conferma = (input('inserire un altro voto? y=yes n=no'))
+    if conferma != 'y':
+        flag=False
+
+print (voti)
+index_max = max(voti)
+index_min = min(voti)
+print  ('il voto migliore è ', index_max)
+print  ('il voto peggiore è ',index_min)
